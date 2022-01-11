7bafa5e553cd3732d3f4014fdd3d8b5bab119568
woockddus42 <96810300+woockddus42@users.noreply.github.com>
2022-01-11 20:27:22 +0900

    Update MEM.v

--- MEM.v
+++ MEM.v
@@ -15,7 +15,6 @@ module MEM(
 //    output [1:0] mem_ctrl_input, // Data memory control input
     output [31:0] address,      // Data memory input
     output [31:0] w_data,        // Data memory input
-    output [31:0] mem_data
 );
 
 reg [2:0] ctrl_wb_reg;
@@ -25,7 +24,6 @@ reg signed [31:0] mem_data_reg;
 reg signed [31:0] alu_data_reg;
 reg [31:0] address_reg;
 reg [31:0] w_data_reg;
-reg [31:0] mem_data_reg;
 
 //ctrl_mem[4] = memread
 //ctrl_mem[3] = memwrite
@@ -40,13 +38,14 @@ begin : REGISTER
         alu_data_reg <= 32'sd0;
         address_reg <= 32'd0;
         w_data_reg <= 32'd0;
-        mem_data_reg <= 32'd0;
     end else begin
         ctrl_wb_reg <= ctrl_mem[2:0];
         rd_wb_reg <= rd_mem;
         pc4_wb_reg <= pc4_mem;
         mem_data_reg <= read_data;
         alu_data_reg <= alu_result;
+        address_reg <= 
+        w_data_reg <= 
     end
 end
 
