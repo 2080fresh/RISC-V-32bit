156e4e697f1493e72479d5f9a3fc810ae7e91bbb
JaeUkPark <96907816+JaeUkPark@users.noreply.github.com>
2022-01-11 17:48:39 +0900

    결과 text file생성

--- WB_tb.v
+++ WB_tb.v
@@ -17,6 +17,7 @@ wire [31:0] write_data;
 wire [31:0] write_addr;
 
 integer FID;
+integer i=0;
 
 WB WB_1(.ctrl_wb(ctrl_wb), .pc4_wb(pc4_wb), .mem_data(mem_data),
         .alu_data(alu_data), .rd_wb(rd_wb),
@@ -36,48 +37,56 @@ always #5 clk = ~clk;
 
 initial
 begin
-    #10 ctrl_wb = 3'b000; pc4_wb = 32'sd1; mem_data = 32'sd2;
-        alu_data = 32'sd3; rd_wb = 32'sd4;
+    #10 ctrl_wb = 3'b000; pc4_wb = 32'sd3; mem_data = 32'sd2;
+        alu_data = 32'sd1; rd_wb = 32'sd4;
         FID = $fopen("result.txt");
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === alu_data))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
-     #10 ctrl_wb = 3'b001;
+	    $fdisplay(FID, "testcase #%2d : fail", i);
+    #10 ctrl_wb = 3'b001;
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === alu_data))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
+	    $fdisplay(FID, "testcase #%2d : fail", i);
     #10 ctrl_wb = 3'b010;
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === mem_data))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
+	    $fdisplay(FID, "testcase #%2d : fail", i);
     #10 ctrl_wb = 3'b011;
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === mem_data))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
+	    $fdisplay(FID, "testcase #%2d : fail", i);
     #10 ctrl_wb = 3'b100;
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === pc4_wb))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
+	    $fdisplay(FID, "testcase #%2d : fail", i);
     #10 ctrl_wb = 3'b101;
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === pc4_wb))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
+	    $fdisplay(FID, "testcase #%2d : fail", i);
     #10 ctrl_wb = 3'b110;
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === pc4_wb))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
+	    $fdisplay(FID, "testcase #%2d : fail", i);
     #10 ctrl_wb = 3'b111;
+    #1  i = i+1;
         if((ctrl_wb[0] === op_write) && (write_data === pc4_wb))
-            $fdisplay(FID, "success");
+            $fdisplay(FID, "testcase #%2d : success", i);
         else
-	    $fdisplay(FID, "fail");
+	    $fdisplay(FID, "testcase #%2d : fail", i);
     #10 $fclose("FID"); $stop;
 end
 
